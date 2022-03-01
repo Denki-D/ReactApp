fce3c2ec5a76b8e7cfec2be7572197db4259aa8d
Denis Gaščić <denisgascic9@gmail.com>
2022-03-02 00:58:31 +0100

    dodane postavke za firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Navbar from "./komponente/Navbar";
 import Prijava from "./stranice/Prijava";
-
+import { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 export const App = () => {
   return (
+      <BrowserRouter>
       <div>
     <Navbar />
+    <Route exact path="/"></Route>
     <Prijava />
     </div>
+    </BrowserRouter>
   )
 }
 
